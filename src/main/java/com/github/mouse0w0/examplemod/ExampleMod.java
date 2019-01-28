1bf0d12bebd93c8c04feb9a25e884c2472c98781
Mouse <mouse0w0@qq.com>
2019-01-28 18:26:08 +0800

    Update

--- src/main/java/com/github/mouse0w0/examplemod/ExampleMod.java
+++ src/main/java/com/github/mouse0w0/examplemod/ExampleMod.java
@@ -1,19 +1,14 @@
 package com.github.mouse0w0.examplemod;
 
-import org.slf4j.Logger;
 import unknowndomain.engine.event.Listener;
 import unknowndomain.engine.event.game.GameReadyEvent;
 import unknowndomain.engine.mod.Mod;
-import unknowndomain.engine.mod.inject.Log;
 
 @Mod("examplemod:1.0.0")
 public class ExampleMod {
 
-    @Log
-    public static Logger logger;
-
     @Listener
     public void onReady(GameReadyEvent event) {
-        logger.info("Hello UDEngine.");
+        System.out.println("Hello UDEngine");
     }
 }
