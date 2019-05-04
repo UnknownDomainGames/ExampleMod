fcee97bbea1851e881cfa9060ac3b0926810b07a
Mouse <mouse0w0@qq.com>
2019-05-04 18:55:24 +0800

    Update

--- src/main/java/com/github/mouse0w0/examplemod/ExampleMod.java
+++ src/main/java/com/github/mouse0w0/examplemod/ExampleMod.java
@@ -1,14 +1,14 @@
 package com.github.mouse0w0.examplemod;
 
 import unknowndomain.engine.event.Listener;
-import unknowndomain.engine.event.game.GameReadyEvent;
+import unknowndomain.engine.event.game.GameEvent;
 import unknowndomain.engine.mod.Mod;
 
 @Mod("examplemod:1.0.0")
 public class ExampleMod {
 
     @Listener
-    public void onReady(GameReadyEvent event) {
+    public void onReady(GameEvent.Ready event) {
         System.out.println("Hello UDEngine");
     }
 }
